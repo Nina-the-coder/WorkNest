1ebcd275875464180d0bbcabfd8816b2a8fd4d90
Deepanshu <n09456791@gmail.com>
2025-07-05 20:50:33 +0530

    Fixed the bug

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import EmployeeManagement from "./pages/EmployeeManagement";
 import TaskManagement from "./pages/TaskManagement";
