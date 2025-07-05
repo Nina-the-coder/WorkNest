221f705f575476dad051fa024aaed93112d2c820
Deepanshu <n09456791@gmail.com>
2025-07-05 20:05:16 +0530

    feat: added register-admin route

--- backEnd/routes/authRoutes.js
+++ backEnd/routes/authRoutes.js
@@ -7,30 +7,31 @@ const bcrypt = require("bcrypt");
 // login
 router.post("/login", loginUser);
 
-router.post("/seed-admin", async (req, res) => {
+router.post("/register-admin", async (req, res) => {
   try {
-    const existing = await User.findOne({ email: "nina@example.com" });
+    const { name, email, password, empId } = req.body;
+
+    const existing = await User.findOne({ email });
     if (existing) {
       return res.status(400).json({ message: "Admin already exists" });
     }
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash("admin123", salt);
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = new User({
-      name: "Nina",
-      email: "nina@example.com",
-      password: hashedPassword,
-      role: "admin", // or "employee"
-      empId: "EMP001",
+      name,
+      email,
+      password,
+      role: "admin",
+      empId,
     });
 
     await user.save();
 
-    res.status(201).json({ message: "Admin user created", user });
+    res.status(201).json({ message: "Admin registered", user });
   } catch (err) {
-    console.error("Seeding error:", err);
+    console.error(err);
     res.status(500).json({ message: "Server error" });
   }
 });
-
-module.exports = router;
