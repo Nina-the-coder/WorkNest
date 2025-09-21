a83dccb28d89f906de913061e5f3db7627eb6157
Deepanshu <n09456791@gmail.com>
2025-09-22 00:10:59 +0530

    ....

--- backEnd/server.js
+++ backEnd/server.js
@@ -18,10 +18,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/employee", employeeRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log("MongoDB connection error...", err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("MongoDB connection error...", err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
