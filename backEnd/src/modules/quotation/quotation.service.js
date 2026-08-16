87f49235c74f14c065b132ef6e928406a9c3c5ba
Deepanshu <n09456791@gmail.com>
2026-08-16 11:54:18 +0530

    making the routes protected

--- backEnd/src/modules/quotation/quotation.service.js
+++ backEnd/src/modules/quotation/quotation.service.js
@@ -1,8 +1,108 @@
 const quotationRepo = require("./quotation.repository");
 const AppError = require("../../utils/AppError");
 
+const QUOTATION_STATUS = [
+  "DRAFT",
+  "SUBMITTED",
+  "APPROVED",
+  "REJECTED",
+  "EXPIRED",
+];
+
 exports.getAllQuotations = async () => {
   return quotationRepo.findAllActive();
+};
+
+exports.getQuotationById = async (quotationId) => {
+  const quotation = await quotationRepo.findActiveByQuotationId(quotationId);
+
+  if (!quotation) {
+    throw new AppError("Quotation not found", 404);
+  }
+
+  return quotation;
+};
+
+exports.createQuotation = async (quotationData) => {
+  const {
+    quotationNumber,
+    customerId,
+    employeeId,
+    subtotal,
+    discountAmount,
+    gstAmount,
+    grandTotal,
+    validUntil,
+    notes,
+  } = quotationData;
+
+  if (!quotationNumber) {
+    throw new AppError("Quotation number is required", 400);
+  }
+
+  if (!customerId) {
+    throw new AppError("Customer is required", 400);
+  }
+
+  if (!employeeId) {
+    throw new AppError("Employee is required", 400);
+  }
+
+  const existingQuotation =
+    await quotationRepo.findByQuotationNumber(quotationNumber);
+
+  if (existingQuotation) {
+    throw new AppError("Quotation number already exists", 409);
+  }
+
+  const quotation = {
+    quotationNumber,
+    customerId,
+    employeeId,
+    status: "DRAFT",
+    subtotal: subtotal || 0,
+    discountAmount: discountAmount || 0,
+    gstAmount: gstAmount || 0,
+    grandTotal: grandTotal || 0,
+    validUntil,
+    notes: notes || "",
+  };
+
+  return quotationRepo.createQuotation(quotation);
+};
+
+exports.updateQuotation = async (quotationId, quotationData) => {
+  const quotation = await quotationRepo.findActiveByQuotationId(quotationId);
+
+  if (!quotation) {
+    throw new AppError("Quotation not found", 404);
+  }
+
+  if (quotation.status !== "DRAFT") {
+    throw new AppError(
+      "Only draft quotations can be updated",
+      400
+    );
+  }
+
+  const allowedFields = [
+    "customerId",
+    "employeeId",
+    "subtotal",
+    "discountAmount",
+    "gstAmount",
+    "grandTotal",
+    "validUntil",
+    "notes",
+  ];
+
+  allowedFields.forEach((field) => {
+    if (quotationData[field] !== undefined) {
+      quotation[field] = quotationData[field];
+    }
+  });
+
+  return quotationRepo.saveQuotation(quotation);
 };
 
 exports.updateQuotationStatus = async (quotationId, status) => {
@@ -12,7 +112,12 @@ exports.updateQuotationStatus = async (quotationId, status) => {
     throw new AppError("Quotation not found", 404);
   }
 
+  if (!QUOTATION_STATUS.includes(status)) {
+    throw new AppError("Invalid quotation status", 400);
+  }
+
   quotation.status = status;
+
   return quotationRepo.saveQuotation(quotation);
 };
 
@@ -28,151 +133,3 @@ exports.deleteQuotation = async (quotationId) => {
 
   return quotationRepo.saveQuotation(quotation);
 };
-
-// exports.getQuotationById = async (quotationId) => {
-//   const quotation = await Quotation.findOne({ quotationId, deleted: false });
-//   if (!quotation) {
-//     throw new AppError("Quotation not found", 404);
-//   }
-//   return quotation;
-// };
-
-
-// exports.listQuotation = async () => {
-//   const quotations = await Quotation.find({ deleted: false })
-//     .populate("customerId", "name address contact email gst company")
-//     .populate("products");
-//   return quotations;
-// };
-
-// exports.getQuotationDetails = async (quotationId) => {
-//   const quotation = await Quotation.findOne({ quotationId, deleted: false })
-//     .populate("customerId", "name address contact email gst company")
-//     .populate("products");
-//   if (!quotation) {
-//     throw new AppError("Quotation not found", 404);
-//   }
-//   return quotation;
-// };
-
-// exports.createQuotation = async (data, userId) => {
-//   const { customerId, products } = data;
-
-//   const newQuotationId = await getNextSequence("quotationId");
-
-//   const newQuotation = new Quotation({
-//     quotationId: newQuotationId,
-//     customerId,
-//     products,
-//     addedBy: userId,
-//     updatedBy: userId,
-//   });
-
-//   await newQuotation.save();
-//   return newQuotation;
-// };
-
-// exports.updateQuotation = async (quotationId, data, userId) => {
-//   const { customerId, products } = data;
-
-//   const quotation = await Quotation.findOne({ quotationId, deleted: false });
-
-//   if (!quotation) {
-//     throw new AppError("Quotation not found", 404);
-//   }
-
-//   if (customerId) quotation.customerId = customerId;
-//   if (products) quotation.products = products;
-
-//   quotation.updatedBy = userId; // who updated
-//   await quotation.save();
-//   return quotation;
-// };
-
-// exports.fetchQuotations = async (req, res) => {
-//   try {
-//     const quotations = await Quotation.find({ deleted: false })
-//       .populate({ path: "addedBy", select: "empId name" })
-//       .populate({ path: "customerId", select: "customerId name" });
-//     res.status(200).json(quotations);
-//   } catch (err) {
-//     console.error("Server errror", err);
-//     res.status(500).json({
-//       message: "SErver eror failed to fetch the quotations from the database",
-//       error: err.message,
-//     });
-//   }
-// };
-
-// exports.getQuotationDetails = async (req, res) => {
-//   try {
-//     const { quotationId } = req.params;
-//     const quotation = await Quotation.findOne({ quotationId, deleted: false })
-//       .populate("customerId", "name address contact email gst company")
-//       .populate("products");
-//     if (!quotation) {
-//       return res.status(404).json({ message: "Quotation not found" });
-//     }
-//     res.status(200).json(quotation);
-//   } catch (err) {
-//     console.error("Server error", err);
-//     res.status(500).json({
-//       message: "Server error failed to fetch the quotation details",
-//       error: err.message,
-//     });
-//   }
-// };
-
-// exports.updateQuotationStatus = async (req, res) => {
-//   try {
-//     const { quotationId } = req.params;
-//     const { status } = req.body;
-
-//     const updatedQuotation = await Quotation.findOneAndUpdate(
-//       { quotationId, deleted: false },
-//       { status },
-//       { new: true },
-//     );
-
-//     if (!updatedQuotation) {
-//       return res.status(404).json({ message: "Quotation not found" });
-//     }
-
-//     res.status(200).json({
-//       message: "updated quotation successfully",
-//       quotation: updatedQuotation,
-//     });
-//   } catch (err) {
-//     console.error("Server error", err);
-//     res.status(500).json({
-//       message: "Server error failed to update the status of the quotation",
-//       error: err.message,
-//     });
-//   }
-// };
-
-// exports.deleteQuotation = async (req, res) => {
-//   try {
-//     const { quotationId } = req.params;
-
-//     const quotation = await Quotation.findOne({ quotationId, deleted: false });
-
-//     if (!quotation) {
-//       return res.status(404).json({ message: "Quotation not found" });
-//     }
-
-//     quotation.deleted = true;
-//     quotation.deletedAt = new Date();
-//     await quotation.save();
-
-//     res.status(200).json({
-//       message: "quotation deleted successfully",
-//     });
-//   } catch (err) {
-//     console.error("Server error", err);
-//     res.status(500).json({
-//       message: "Server error failed to delete the quotation",
-//       error: err.message,
-//     });
-//   }
-// };
